6e71071337f1d7731daa50cf7271f2cbacc963c0
weiyt <75768623@qq.com>
2017-11-24 10:56:48 +0800

    11/22

--- taotao-manage/taotao-manage-service/src/main/java/com/taotao/manage/service/ItemService.java
+++ taotao-manage/taotao-manage-service/src/main/java/com/taotao/manage/service/ItemService.java
@@ -13,53 +13,54 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class ItemService extends BaseService<Item>{
+public class ItemService extends BaseService<Item> {
 
-	@Autowired
-	private ItemMapper itemMapper;
-	@Autowired
-	private ItemDescService itemDescService;
+    @Autowired
+    private ItemMapper itemMapper;
+    @Autowired
+    private ItemDescService itemDescService;
 
-	/**
-	 * 保存商品和商品描述
-	 * @param item
-	 * @param desc
-	 * @return
-	 */
-	public Boolean saveItemAndDesc(Item item,String desc){
+    /**
+     * 保存商品和商品描述
+     *
+     * @param item
+     * @param desc
+     * @return
+     */
+    public Boolean saveItemAndDesc(Item item, String desc) {
 
-		item.setId(null);
-		item.setStatus(1);
-		//保存商品数据
-		Integer saveItemCount = super.save(item);
-		if(saveItemCount.intValue() == 0){
-			//数据插入失败  但是没有任何异常
-			throw new RuntimeException(" 新增商品信息失败，但是没有异常。Item ="+item);
-		}
-		Integer saveDescCount = 0;
-		ItemDesc itemDesc = new ItemDesc();
-		if(saveItemCount.intValue() == 1){
-			itemDesc.setItemId(item.getId());
-			itemDesc.setItemDesc(desc);
-			saveDescCount = this.itemDescService.save(itemDesc);
-		}
-		if(saveDescCount.intValue() == 0){
-			//数据插入失败  但是没有任何异常
-			throw new RuntimeException(" 新增商品描述信息失败，但是没有异常。ItemDesc ="+itemDesc);
-		}
-		if(saveItemCount.intValue() == 1 && saveDescCount.intValue() == 1){
-			return true;
-		}
-		return false;
-	}
+        item.setId(null);
+        item.setStatus(1);
+        //保存商品数据
+        Integer saveItemCount = super.save(item);
+        if (saveItemCount.intValue() == 0) {
+            //数据插入失败  但是没有任何异常
+            throw new RuntimeException(" 新增商品信息失败，但是没有异常。Item =" + item);
+        }
+        Integer saveDescCount = 0;
+        ItemDesc itemDesc = new ItemDesc();
+        if (saveItemCount.intValue() == 1) {
+            itemDesc.setItemId(item.getId());
+            itemDesc.setItemDesc(desc);
+            saveDescCount = this.itemDescService.save(itemDesc);
+        }
+        if (saveDescCount.intValue() == 0) {
+            //数据插入失败  但是没有任何异常
+            throw new RuntimeException(" 新增商品描述信息失败，但是没有异常。ItemDesc =" + itemDesc);
+        }
+        if (saveItemCount.intValue() == 1 && saveDescCount.intValue() == 1) {
+            return true;
+        }
+        return false;
+    }
 
 
     public EasyUIResult queryPage(Integer pageNum, Integer pageSize) {
-		PageHelper.startPage(pageNum,pageSize);
-		Example example = new Example(Item.class);
-		example.setOrderByClause("updated desc");
-		List<Item> items =itemMapper.selectByExample(example);
-		PageInfo<Item> page = new PageInfo<>(items);
-		return new EasyUIResult(page.getTotal(),items);
-	}
+        PageHelper.startPage(pageNum, pageSize);
+        Example example = new Example(Item.class);
+        example.setOrderByClause("updated desc");
+        List<Item> items = itemMapper.selectByExample(example);
+        PageInfo<Item> page = new PageInfo<>(items);
+        return new EasyUIResult(page.getTotal(), items);
+    }
 }
